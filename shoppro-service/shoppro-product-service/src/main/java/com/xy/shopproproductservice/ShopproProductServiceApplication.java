c1d39153d67db3c0f6a06dfc7a481a3bddb61b49
songyingqi <942763405@qq.com>
2019-08-03 09:30:29 +0800

    第一次生成SpringBootDubbo项目骨架

---
+++ shoppro-service/shoppro-product-service/src/main/java/com/xy/shopproproductservice/ShopproProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.xy.shopproproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopproProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopproProductServiceApplication.class, args);
+    }
+
+}
---
+++ shoppro-web/shoppro-background/src/main/java/com/xy/shopprobackground/ShopproBackgroundApplication.java
@@ -0,0 +1,13 @@
+package com.xy.shopprobackground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopproBackgroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopproBackgroundApplication.class, args);
+    }
+
+}
