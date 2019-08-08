729c9c6752db23fce91bcf0a06f57822d91151ff
songyingqi <942763405@qq.com>
2019-08-08 21:34:57 +0800

    成功创建了dubbo体系，正常的数据交互，现在正在做首页系统

---
+++ shoppro-web/shoppro-index/src/main/java/com/xy/shopproindex/ShopproIndexApplication.java
@@ -0,0 +1,13 @@
+package com.xy.shopproindex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopproIndexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopproIndexApplication.class, args);
+    }
+
+}
